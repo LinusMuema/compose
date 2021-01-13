c1aa1d27c8de72445b0492d2da3b52992df95b00
linus muema <linus.m.muema@gmail.com>
2021-01-13 20:20:07 +0300

    - changed the theme

--- app/src/main/java/com/moose/micompose/MainActivity.kt
+++ app/src/main/java/com/moose/micompose/MainActivity.kt
@@ -4,28 +4,25 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.unit.dp
-import androidx.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.moose.micompose.theme.AppTheme
 import com.moose.micompose.ui.BodyContent
 import com.moose.micompose.ui.BodyState
-import com.moose.micompose.ui.BottomNavigation
 
 class MainActivity : AppCompatActivity() {
     private var width = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         width = resources.displayMetrics.run { widthPixels / density }.toInt()
         setContent {
-            AppTheme() {
+            AppTheme {
                 Content()
             }
         }
@@ -36,15 +33,6 @@ class MainActivity : AppCompatActivity() {
         val activity = remember { mutableStateOf(BodyState.HOME) }
         Box(modifier = Modifier.fillMaxSize()){
             BodyContent(activity)
-            Box(modifier = Modifier.align(Alignment.BottomEnd).padding(10.dp)){
-                BottomNavigation (width = width, activity = activity)
-            }
         }
-    }
-
-    @Preview("Content")
-    @Composable
-    fun preview() {
-        Content()
     }
 }
