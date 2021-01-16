7a37eb52b4c64d7fb80afd886e8e8a173828695d
linus muema <linus.m.muema@gmail.com>
2021-01-16 10:21:54 +0300

    - commit latest changes

--- app/src/main/java/com/moose/traveller/components/buttons.kt
+++ app/src/main/java/com/moose/traveller/components/buttons.kt
@@ -1,8 +1,23 @@
 package com.moose.traveller.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun SplashButton(text: String){
+    Box(modifier = Modifier.clip(MaterialTheme.shapes.medium)) {
+        Text(text = text, color = Color.White)
+    }
+}
 
+@Preview
+@Composable
+fun PreviewButton(){
+    SplashButton(text = "Log in")
 }
