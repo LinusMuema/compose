c113f9c6584c63f158020cfa78196e8a1a74962b
linus muema <linus.m.muema@gmail.com>
2021-03-16 19:02:04 +0300

    - added the theme

--- app/src/main/java/com/moose/traveller/theme/Color.kt
+++ app/src/main/java/com/moose/traveller/theme/Color.kt
@@ -1,6 +1,8 @@
 package com.moose.traveller.theme
 
+import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.ui.graphics.Color
 
-val colors = lightColors(primary = Color.White)
+val light = lightColors(primary = Color.White)
+val dark = darkColors()
--- app/src/main/java/com/moose/traveller/theme/Theme.kt
+++ app/src/main/java/com/moose/traveller/theme/Theme.kt
@@ -1,10 +1,12 @@
 package com.moose.traveller.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 
 
 @Composable
-fun TravellerTheme(content: @Composable () -> Unit) {
+fun TravellerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) dark else light
     MaterialTheme(colors = colors, typography = typography, shapes = shapes, content = content)
 }
